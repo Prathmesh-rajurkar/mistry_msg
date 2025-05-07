84eca12e2dbe36e849057bb8204e6282f79ed9d8
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2025-05-07 14:40:52 +0530

    api

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -6,8 +6,72 @@ import bcrypt from "bcryptjs";
 export async function POST(request: Request) {
   await dbConnect();
   try {
-    const {username,password,email} = await request.json();
-    
+    const { username, password, email } = await request.json();
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username already exists",
+        },
+        { status: 400 }
+      );
+    }
+    const existingUserVerifiedByEmail = await UserModel.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUserVerifiedByEmail) {
+      return Response.json(
+        {
+          success: false,
+          message: "Email already exists",
+        },
+        { status: 400 }
+      );
+    }else {
+        const hashedPassword = await bcrypt.hash(password,10);
+        const expiryDate = new Date();
+        expiryDate.setHours(expiryDate.getHours() + 1);
+
+        const newUser = new UserModel({
+            username,
+            password: hashedPassword,
+            email,
+            isVerified: false,
+            verifyCode,
+            verifyCodeExpired: expiryDate,
+            isAcceptingMessages: true,
+            messages: []
+        })
+
+        await newUser.save();
+    }
+
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User registered successfully",
+      },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error in registering user: ", error);
     return Response.json(
